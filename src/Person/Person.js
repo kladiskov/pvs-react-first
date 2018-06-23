4f916dd4d82d69da5af80ec0eb520b64aceb3fb4
kladiskov <sandesh2k17@gmail.com>
2018-06-23 12:33:59 +0530

    Css Modules added with pseudo selectors

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
     const style = {
@@ -8,7 +8,7 @@ const person = (props) => {
             width: '450px'
         }
     }
-    return (<div className="Person" style={style}>
+    return (<div className={classes.Person} style={style}>
         <p onClick={props.click}> Hi, I am {props.name} and I am {props.age} years old</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name} />
